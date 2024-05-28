a8c89b26dd8d1af9702d54cf6a06bef058fbbaf7
JozzyBRNSN <makavelidmityr313@gmail.com>
2024-05-28 23:40:29 +0300

    added classes

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
-function newCard(number, action) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.textContent = number;
-  document.getElementById("game").append(card);
+class Card {
+  _open = false;
+  _success = false;
 
-  card.addEventListener("click", function () {
-    card.classList.add("open");
-    action(card);
-  });
-};
+  constructor(container, number, action) {
+    this.card = document.createElement("div");
+    this.card.classList.add("card");
+    this.card.textContent = number;
 
-// callback функция
+    this.card.addEventListener("click", () => {
+      if (this.open === false && this.success === false) {
+        this.open = true;
+        action(this);
+      }
+    });
+    container.append(this.card);
+  }
 
-function flip(card) {
-  card.classList.add("success");
+  set open(value) {
+    this._open = value;
+    value
+      ? this.card.classList.add("open")
+      : this.card.classList.remove("open");
+  }
+
+  get open() {
+    return this._open;
+  }
+
+  set success(value) {
+    this._success = value;
+    value
+      ? this.card.classList.add("success")
+      : this.card.classList.remove("success");
+  }
+
+  get success() {
+    return this._success;
+  }
 }
 
-newCard(1, flip);
-newCard(2);
-newCard(3);
-newCard(4);
+let newCard = new Card(document.getElementById("game"), "1", flip);
+let newCard2 = new Card(document.getElementById("game"), "1", flip);
+let newCard3 = new Card(document.getElementById("game"), "1", flip);
+let newCard4 = new Card(document.getElementById("game"), "1", flip);
+
+// callback функция
+function flip(card) {
+  console.log(card);
+}
